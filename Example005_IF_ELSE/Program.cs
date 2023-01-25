0aa833d3e2aa365cde222dfba084e82b71788776
ilyaBobrov <rog89@bk.ru>
2023-01-25 21:11:39 +0500

    Task VIP Hello

---
+++ Example005_IF_ELSE/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Write ("Введите имя пользователя ");
+string name = Console.ReadLine();
+if(name.ToLower() == "маша")
+{
+    Console.WriteLine("УРА! Это же Маша");
+}
+else
+{
+    Console.Write("Привет, ");
+    Console.Write(name);
+}
