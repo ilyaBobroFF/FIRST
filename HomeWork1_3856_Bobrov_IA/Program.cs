eb874da4cf7f092f722ec64e41d1a0c4fd7e68a1
ilyaBobrov <rog89@bk.ru>
2023-01-27 11:32:33 +0500

    Task 2

--- HomeWork1_3856_Bobrov_IA/Program.cs
+++ HomeWork1_3856_Bobrov_IA/Program.cs
@@ -4,10 +4,11 @@
 // a = 2 b = 10 -> max = 10
 // a = -9 b = -3 -> max = -3
 
-Console.WriteLine("Enter first number ");
+Console.WriteLine("Enter first number for Task_1");
 int number1 = Convert.ToInt32(Console.ReadLine());
-Console.WriteLine("Enter second number");
+Console.WriteLine("Enter second number for Task_1");
 int number2 = Convert.ToInt32(Console.ReadLine());
+
 if (number1 > number2)
 {
     Console.Write("Max number = ");
@@ -26,4 +27,36 @@ else
     Console.WriteLine(number2);
     Console.Write("Min number = ");
     Console.WriteLine(number1); 
+}
+// Задача 4: Напишите программу, которая принимает на вход
+// три числа и выдает максимальное из этих чисел.
+// 2, 3, 7 -> 7
+// 44 5 78 -> 78
+// 22 3 9 -> 22
+
+Console.WriteLine("Enter first number for Task_2");
+int number3 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Enter second number for Task_2");
+int number4 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Enter third number for Task_2");
+int number5 = Convert.ToInt32(Console.ReadLine());
+
+if (number3 > number4)
+{
+    if(number3 > number5)
+    {
+        Console.Write("Max number = ");
+        Console.WriteLine(number3);
+    }
+   
+}
+else if(number4 > number5)
+{
+    Console.Write("Max number = ");
+    Console.WriteLine(number4);
+}
+else
+{
+    Console.Write("Max number = ");
+    Console.WriteLine(number5); 
 }
